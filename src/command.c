a60b9af4470ca6413e17cfda5577d8adcd45f958
Alexandros Papagrigoriou <apapagrigoriou17@gmail.com>
2026-04-10 03:27:28 +0300

    Merged change into delete + insert mode

--- src/command.c
+++ src/command.c
@@ -57,7 +57,6 @@ static void handleOperatorNone(Parser* parser, Editor* editor) {
 }
 
 static void handleOperatorDelete(Parser* parser, Editor* editor) {
-    // TODO: add count in commands
     switch (parser->cmd.motion) {
         case MOT_LEFT:
             deleteCharLeft(&editor->buffer, &editor->cursor, parser->cmd.count);
@@ -69,10 +68,42 @@ static void handleOperatorDelete(Parser* parser, Editor* editor) {
         case MOT_RIGHT:
             deleteCharRight(&editor->buffer, &editor->cursor, parser->cmd.count);
             break;
-        case MOT_WORD:
-            break;
         case MOT_LINE:
             deleteLine(&editor->buffer, &editor->cursor, parser->cmd.count);
+            break;
+        case MOT_WORD:
+            break;
+        case MOT_BIG_WORD:
+            break;
+        case MOT_END_WORD:
+            break;
+        case MOT_END_BIG_WORD:
+            break;
+        case MOT_BACKWARD_WORD:
+            break;
+        case MOT_BACKWARD_BIG_WORD:
+            break;
+        case MOT_START_OF_LINE:
+            break;
+        case MOT_FIRST_NON_BLANK_CHAR_OF_LINE:
+            break;
+        case MOT_END_OF_LINE:
+            break;
+        case MOT_FIRST_LINE:
+            break;
+        case MOT_LAST_LINE:
+            break;
+        case MOT_NEXT_PARAGRAPH:
+            break;
+        case MOT_PREVIOUS_PARAGRAPH:
+            break;
+        case MOT_NEXT_OCCURRENCE_OF_CHAR:
+            break;
+        case MOT_BEFORE_NEXT_OCCURRENCE_OF_CHAR:
+            break;
+        case MOT_PREVIOUS_OCCURRENCE_OF_CHAR:
+            break;
+        case MOT_AFTER_PREVIOUS_OCCURRENCE_OF_CHAR:
             break;
         default:
             break;
@@ -80,14 +111,8 @@ static void handleOperatorDelete(Parser* parser, Editor* editor) {
 }
 
 static void handleOperatorChange(Parser* parser, Editor* editor) {
-    switch (parser->cmd.motion) {
-        case MOT_LINE:
-            deleteLine(&editor->buffer, &editor->cursor, parser->cmd.count);
-            editor->mode = INSERT;
-            break;
-        default:
-            break;
-    }
+    handleOperatorDelete(parser, editor);
+    editor->mode = INSERT;
 }
 
 static void handleOperatorYank(Parser* parser, Editor* editor) {
